49f6dc66e0e794e529d8a45b3df1caa53ce11ed9
vinaymatcha <m.vinay38@gmail.com>
2019-02-17 15:42:01 -0700

    Deep Learning Basics - Image files

--- basics/EcommerceSklearn.py
+++ basics/EcommerceSklearn.py
@@ -1,7 +1,5 @@
 from basics.process import get_data_check
 from sklearn.neural_network import MLPClassifier
-from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split
 def main():
     X_train, Y_train, X_test, Y_test = get_data_check()
     model = MLPClassifier(max_iter=2000)
